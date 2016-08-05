4e3179193b1585d41d3c78288b81581af88627f6
Bright <brighttrieu@hotmail.com>
2016-08-05 13:34:38 -0700

    update layout of product page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160804014907) do
+ActiveRecord::Schema.define(version: 20160805052917) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20160804014907) do
     t.string   "textimage_url"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.string   "color"
+    t.string   "colour"
     t.integer  "price"
   end
 
