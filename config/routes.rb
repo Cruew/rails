0770b91d373e20f013f146c19ef81e11214d93c0
Bright <brighttrieu@hotmail.com>
2016-08-17 13:21:33 -0700

    5.1 updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   resources :products
+
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
   get 'static_pages/index'
+
+  get 'static_pages/landing_page'
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
@@ -12,7 +15,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-    root 'static_pages#landing_page'
+    root 'static_pages#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
