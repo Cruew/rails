8c78209da3d804ddf698e717d99583dde93b411d
Bright <brighttrieu@hotmail.com>
2016-08-19 01:57:43 -0700

    5.2 included search bar and styled product description

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,12 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+	$("#search-bar").hide();
+    $("#search").click(function(event) {
+        event.preventDefault();
+        $("#search-bar").toggle();
+    });
+});
+
